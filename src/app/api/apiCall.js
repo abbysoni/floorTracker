a2dbc68a0121f851de6149937b8da404e0b38258
Testabby <a9094855@gmail.com>
2024-01-24 23:47:35 +0530

    Stored VercelApiToken in env and get and patch function worked

--- src/app/api/apiCall.js
+++ src/app/api/apiCall.js
@@ -1,6 +1,135 @@
+// USING EDGE CONFIG ENDPOINTS
 
-// const update = async()=>{
-//   try {
+// https://edge-config.vercel.com/
+// ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?
+// token=f3e0befb-6581-4020-8585-02b8ae03ca10
+
+// To get the data from edge config 
+// const showData = async ()=>{
+//     try {
+//     // get data directly from api
+//     let response = await fetch('https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10',
+//       {
+//                 method: 'GET',
+//                 headers: {
+//                   Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+//                   // Content-Type: 'application/json',
+//                 },
+//               })
+//               if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+  
+//      const result = await response.json();
+//     console.log(result);
+//     // console.log(result);
+//     setdata(result.items)
+
+//   }catch(err) {
+//     setError (err.message)
+//   }
+//   };
+
+// Request all items
+// https://edge-config.vercel.com/
+// ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?
+// token=f3e0befb-6581-4020-8585-02b8ae03ca10
+// 'https://edge-config.vercel.com/your_edge_config_id_here/items?token=your_edge_config_read_access_token_here',
+
+// const getAllData = async() => {
+//     try {
+//         const readAllEdgeConfigItems = await fetch(
+//           'https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn/items?token=f3e0befb-6581-4020-8585-02b8ae03ca10',
+//         );
+//         const result = await readAllEdgeConfigItems.json();
+//         console.log(result);
+//       } catch (error) {
+//         console.log(error);
+//       }
+// }
+
+// OOORRRR send auth in header 
+
+// const getAllData = async() => {
+//    try {
+//   const readAllWithAuth = await fetch(
+//     'https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn/items',
+//     {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+//       },
+//     },
+//   );
+//   const result = await readAllWithAuth.json();
+//   console.log(result);
+// } catch (error) {
+//   console.log(error);
+// }
+// }
+
+// Read single item
+// try {
+//     const readSingle = await fetch(
+//       'https://edge-config.vercel.com/your_edge_config_id_here/item/example_key_1?token=your_edge_config_read_access_token_here',
+//     );
+//     const result = await readSingle.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// Read edge config digest 
+// try {
+//     const readDigest = await fetch(
+//       'https://edge-config.vercel.com/your_edge_config_id_here/digest?teamId=your_team_id_here&token=your_edge_config_read_access_token_here',
+//     );
+//     const result = await readDigest.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USING VERCEL API ENDPOINTS
+// The auth token used here is diffrent from what is used with above apis
+// eyUyJwDlIMAKkT4OWvqu0jWY
+// Need to create this from account setting > create token
+
+// To update your edge config items
+// To add an item to or update an item in your Edge Config, send a PATCH request to the edge-config endpoint, appending /your_edge_config_id_here/items to the end.
+
+// 'https://api.vercel.com/v1/edge-config/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10/timer
+// {
+//   "items": [
+//     {
+//       "operation": "create",
+//       "key": "example_key_1",
+//       "value": "example_value_1"
+//     },
+//     {
+//       "operation": "update",
+//       "key": "example_key_2",
+//       "value": "new_value"
+//     }
+//   ]
+// }
+
+// try {
 //     const updateEdgeConfig = await fetch(
 //       'https://api.vercel.com/v1/edge-config/your_edge_config_id_here/items',
 //       {
@@ -30,32 +159,148 @@
 //   } catch (error) {
 //     console.log(error);
 //   }
-// };
+
+// try {
+//     const updateEdgeConfig = await fetch(
+//       'https://api.vercel.com/v1/edge-config/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10/timer',
+//       {
+//         method: 'PATCH',
+//         headers: {
+//           Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           items: [
+//             {
+//               operation: 'create',
+//               key: 'timer',
+//               value: '30',
+//             },
+
+//           ],
+//         }),
+//       },
+//     );
+//     const result = await updateEdgeConfig.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
 
 
+// Read All items from edge config with api.vercel.com
+// 'https://api.vercel.com/v1/edge-config/your_edge_config_id_here/items';
+// ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10
 
-// const result = await fetch(
-// 	'https://api.vercel.com/v1/edge-config/your_edge_config_id_here/items',
-// 	{
-// 		method: 'PATCH',
-// 		headers: {
-// 			Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
-// 			'content-type': 'application/json',
-// 		},
-// 		body: JSON.stringify({
-// 			items: [
-// 				{
-// 					operation: 'update',
-// 					key: 'greeting',
-// 					value: 'hello vercel',
-// 				},
-// 			],
-// 		}),
-// 	}
-// );
-// const json = await result.json();
-// if (json.status !== 'ok') {
-// 	console.error('Failed to update edge config', json);
+// try {
+//     const readItems = await fetch(
+//       'https://api.vercel.com/v1/edge-config/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10/items',
+//       {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+//         },
+//       },
+//     );
+//     const result = await readItems.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+
+// List all edge configs
+// try {
+//     const listItems = await fetch(
+//       'https://api.vercel.com/v1/edge-config?teamId=your_team_id_here',
+//       {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${your_vercel_api_token_here}`,
+//         },
+//       },
+//     );
+//     const result = await listItems.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+
+//   Read metadata of edge config
+// try {
+//     const readMetadata = await fetch(
+//       'https://api.vercel.com/v1/edge-config/your_edge_config_id_here?teamId=your_team_id_here',
+//       {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${your_vercel_api_token_here}`,
+//         },
+//       },
+//     );
+//     const result = await readMetadata.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// export {
+//     // update, 
+//     // result,
 // }
 
-// export {update, result}
+
+// working set of functions for read and write
+// const showData = async () => {
+//     try {
+//       let response = await fetch('https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn',
+//         {
+//           method: 'GET',
+//           headers: {
+//             Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+//             // Content-Type: 'application/json',
+//           },
+//         })
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+
+//       const result = await response.json();
+//       console.log(result);
+//       // console.log(result);
+//       setdata(result.items)
+
+//     } catch (err) {
+//       setError(err.message)
+//     }
+//   };
+
+// const addData = async () => {
+//     try {
+//       let response = await fetch('https://api.vercel.com/v1/edge-config/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn/items',
+//         {
+//           method: 'PATCH',
+//           headers: {
+//             Authorization: `Bearer ${'eyUyJwDlIMAKkT4OWvqu0jWY'}`,
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({
+//                       items: [
+//                         {
+//                           operation: 'create',
+//                           key: 'example_key_1',
+//                           value: 'example_value_1',
+//                         },
+//                       ],
+//                     }),
+//         })
+
+//       const result = await response.json();
+//       console.log(result);
+//       // console.log(result);
+//       setdata(result)
+
+//     } catch (err) {
+//       setError(err.message)
+//     }
+//   };
--- src/app/api/page.js
+++ src/app/api/page.js
@@ -2,59 +2,117 @@
 import Counter from './counter'
 import Link from 'next/link'
 import { useState } from 'react';
-import { GET } from './get-example/route';
+import { useEffect } from 'react';
 
 
 
-  
+
 export default function ApiCalls() {
-  console.log(process.env.EDGE_CONFIG)
+  // console.log(process.env.EDGE_CONFIG)
   const [data, setdata] = useState([]);
+  const [error, setError] = useState(null);
 
-  const showData= async ()=>{
+  // useEffect(() => {
+  // older code working with edge-config.vercel.com but not with api.vercel.com
+  const showData = async () => {
+    try {
+      let response = await fetch('https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
+            // Content-Type: 'application/json',
+          },
+        })
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-    // get data directly from api
-    let Rdata = await fetch('https://edge-config.vercel.com/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn?token=f3e0befb-6581-4020-8585-02b8ae03ca10',
-      {
-                method: 'GET',
-                headers: {
-                  Authorization: `Bearer ${'f3e0befb-6581-4020-8585-02b8ae03ca10'}`,
-                  // Content-Type: 'application/json',
-                },
-              })
-      // {
-      //   "headers": {
-      //     "Authorization": "Bearer f3e0befb-6581-4020-8585-02b8ae03ca10"
-      //   },
-      //   "method": "get"
-      // })
-     let adata= Rdata.json()
-    console.log(adata);
-    console.log(Rdata.Object);
+      const result = await response.json();
+      console.log(result);
+      // console.log(result);
+      setdata(result.items)
 
-    
-    // let Rdata = await GET();
-    // Rdata = Rdata.json();
-     setdata(Rdata.items);
-  }
+    } catch (err) {
+      setError(err.message)
+    }
+  };
+  //   showData();
+  // },[]);
 
-    return(
+  const addData = async () => {
+    try {
+      let response = await fetch('https://api.vercel.com/v1/edge-config/ecfg_1qhigiwu3auqiyfyw7rttzxfxsjn/items',
+        {
+          method: 'PATCH',
+          headers: {
+            Authorization: `Bearer ${'eyUyJwDlIMAKkT4OWvqu0jWY'}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+                      items: [
+                        {
+                          operation: 'create',
+                          key: 'example_key_1',
+                          value: 'example_value_1',
+                        },
+                      ],
+                    }),
+        })
+
+      const result = await response.json();
+      console.log(result);
+      // console.log(result);
+      setdata(result)
+
+    } catch (err) {
+      setError(err.message)
+    }
+  };
+
+  return (
     <div>
-        <Link href="/">Go To home tab</Link>
-        
-        {/* <Counter/> */}
-        <button
+      <Link href="/">Go To home tab</Link>
+
+      {/* <Counter/> */}
+      <button
         //   className={styles.card}
-          style={{ padding: 20, backgroundColor: 'rgba(var(--card-rgb), 0.2)', fontSize: '20px', marginRight: '50px' }}
-          // onClick={()=> writeToEdgeConfig()}
-           onClick={()=> showData()}
-        >Check API Calls &gt;</button>
-        {(data)?
-        <div>{data}</div>:
-        <div>None data found</div>}
+        style={{ padding: 20, backgroundColor: 'rgba(var(--card-rgb), 0.2)', fontSize: '20px', margin: '50px' }}
+        // onClick={()=> writeToEdgeConfig()}
+        onClick={() => showData()}
+      >showDATA &gt;</button>
+
+      <div>
+        <h1>GET DATA RESPONSE</h1>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {data && (
+          <div>
+            <h2>Data received:</h2>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+
+      <button
+        //   className={styles.card}
+        style={{ padding: 20, backgroundColor: 'rgba(var(--card-rgb), 0.2)', fontSize: '20px', margin: '50px' }}
+        // onClick={()=> writeToEdgeConfig()}
+        onClick={() => addData()}
+      >addDATA &gt;</button>
+
+      <div>
+        <h1>ADD DATA RESPONSE</h1>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {data && (
+          <div>
+            <h2>Data received:</h2>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
+      </div>
 
 
-        
+
     </div>
-    )
+  )
 }
